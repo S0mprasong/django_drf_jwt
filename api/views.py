98efb46c7f62434fcaf79afe9acc47a3a8519f5a
Somprasong <somprasongt.th@gmail.com>
2016-06-08 10:49:48 +0700

    Add API Serializer

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserProfile(APIView):
+    
+    def get(self,request):
+		user = User.objects.get(id=request.user.id)
+		serializer = UserSerializer(user, many=False)
+		return Response(serializer.data)
